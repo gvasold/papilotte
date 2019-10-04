4a394ff1786fcc890335ee011166d0d2ad63cd77
Gunter Vasold <gunter@vasold.org>
2019-10-04 19:50:46 +0200

    Add test for validation of sortdate

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -18,6 +18,30 @@ def test_validate_full(mock_factoid):
     "Validate a full factoid."
     validate(mock_factoid)
 
+def test_validate_sortdate(mock_factoid):
+    "Sortdate defines a regex pattern which should be tested, too"
+    # year only is valid
+    mock_factoid['statement']['date']['sortdate'] = '1899'
+    validate(mock_factoid)
+    # year-month is valid
+    mock_factoid['statement']['date']['sortdate'] = '1899-05'
+    validate(mock_factoid)
+    # year-month-day is valid
+    mock_factoid['statement']['date']['sortdate'] = '1899-05-12'
+    validate(mock_factoid)
+    # yyyy-00-dd is invalid
+    mock_factoid['statement']['date']['sortdate'] = '1899-00-12'
+    with pytest.raises(ValidationError):
+        validate(mock_factoid)
+    # yyyy-mm-00 is invalid
+    mock_factoid['statement']['date']['sortdate'] = '1899-05-00'
+    with pytest.raises(ValidationError):
+        validate(mock_factoid)
+    # yyyy-00-00 is invalid
+    mock_factoid['statement']['date']['sortdate'] = '1899-00-00'
+    with pytest.raises(ValidationError):
+        validate(mock_factoid)
+
 
 def test_missing_source_id(mock_factoid):
     "Validation an invalid factoid must raise a ValidationError."
