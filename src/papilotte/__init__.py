51e19b017c8121f7cf5f0616ddd1e8cf05103453
Gunter Vasold <gunter@vasold.org>
2020-03-06 10:47:29 +0100

    Set minimal Python version to 3.6

--- src/papilotte/__init__.py
+++ src/papilotte/__init__.py
@@ -7,6 +7,6 @@ logger = logging.getLogger("papilotte")
 
 
 if sys.version_info.major < 3 or (
-    sys.version_info.major == 3 and sys.version_info.minor < 5):
-    print("Papilotte requires Python version 3.5 or higher.")
+    sys.version_info.major == 3 and sys.version_info.minor < 6):
+    print("Papilotte requires Python version 3.6 or higher.")
     sys.exit(1)
